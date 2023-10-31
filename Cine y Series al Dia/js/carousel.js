261b1d6f48d9f246e7932e1891b7644cd662b5f2
CrnelAnurdin <149343653+CrnelAnurdin@users.noreply.github.com>
2023-10-31 20:47:27 -0300

    Add files via upload

--- Cine y Series al Dia/js/carousel.js
+++ Cine y Series al Dia/js/carousel.js
@@ -1,36 +1,42 @@
 document.querySelectorAll(".carousel").forEach((carousel) => {
-    const items = carousel.querySelectorAll(".carousel_item");
-    const buttonsHtml = Array.from(items, () => {
-      return `<span class="carousel__button"></span>`;
+  const items = carousel.querySelectorAll(".carousel_item");
+  const buttonsHtml = Array.from(items, () => {
+    return `<span class="carousel__button"></span>`;
+  });
+
+  carousel.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="carousel__nav">
+          ${buttonsHtml.join("")}
+      </div>
+    `
+  );
+
+  const buttons = carousel.querySelectorAll(".carousel__button");
+  let currentIndex = 0; 
+
+  function showItem(index) {
+    items.forEach((item) => item.classList.remove("carousel_item--selected"));
+    buttons.forEach((button) => button.classList.remove("carousel__button--selected"));
+
+    items[index].classList.add("carousel_item--selected");
+    buttons[index].classList.add("carousel__button--selected");
+  }
+
+  function nextItem() {
+    currentIndex = (currentIndex + 1) % items.length; 
+    showItem(currentIndex);
+  }
+
+  buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+      currentIndex = i;
+      showItem(i);
     });
+  });
   
-    carousel.insertAdjacentHTML(
-      "beforeend",
-      `
-          <div class="carousel__nav">
-              ${buttonsHtml.join("")}
-          </div>
-      `
-    );
-  
-    const buttons = carousel.querySelectorAll(".carousel__button");
-  
-    buttons.forEach((button, i) => {
-      button.addEventListener("click", () => {      
-        items.forEach((item) =>
-          item.classList.remove("carousel_item--selected")
-        );
-        buttons.forEach((button) =>
-          button.classList.remove("carousel__button--selected")
-        );
-  
-        items[i].classList.add("carousel_item--selected");
-        button.classList.add("carousel__button--selected");
-      });
-    });
-  
-    
-      items[0].classList.add("carousel_item--selected");
-      buttons[0].classList.add("carousel__button--selected");
-      });
+  setInterval(nextItem, 5000);  
+  showItem(currentIndex);
+});
   
